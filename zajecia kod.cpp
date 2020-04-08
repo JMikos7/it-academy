d96254da7514e0d89c1aa51e7845020d92c1201b
JMikos7 <kubamikos@interia.eu>
2020-04-08 19:31:53 +0200

    Zmiany

---
+++ zajecia kod.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+   int arr[]={4,5,6,1,2,92,321,51251,12313,21};
+   int n = sizeof(arr)/4;
+      for (int i = 0; i < n;i++){
+            cout << arr[i] <<", ";
+      }
+
+
+}
